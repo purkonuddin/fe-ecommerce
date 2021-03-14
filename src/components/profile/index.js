9053e95088a921ca6dfead6e11cfc7fe975fab5e
purkonuddin <purkonud12119617@gmail.com>
2021-03-14 23:39:27 +0700

    profile

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -25,7 +25,7 @@ class Profile extends React.Component {
       <NavbarComp/>
 
       <div className="profile-container" id="profile">  
-          <Tabs selected={ 0 }>
+          <Tabs  selected={ 0 }>
             <TabList>
                 <TabHeader/>
               <Tab>
@@ -44,11 +44,11 @@ class Profile extends React.Component {
             </TabPanel>
 
             <TabPanel>
-              Related to bar
+              <Address/>
             </TabPanel>
 
             <TabPanel>
-              Related to baz
+              <MyOrder/>
             </TabPanel>
           </Tabs> 
       </div>
@@ -166,65 +166,134 @@ class Tabs extends React.Component {
   }
 }
 
+const MyOrder = () => {
+  return (
+    <div className="wrap-right-content">
+      <div>
+        <h3>My order</h3>
+      </div>
+      <div className="rc-center" id="my-order">
+        <Tabs selected={ 0 }>
+          <TabList>
+            <Tab>
+              <TabButton>My Order</TabButton>
+            </Tab>
+            <Tab>
+              <TabButton>Process</TabButton>
+            </Tab>
+            <Tab>
+              <TabButton>Received</TabButton>
+            </Tab>
+            <Tab>
+              <TabButton>Received</TabButton>
+            </Tab>
+            <Tab>
+              <TabButton>Received</TabButton>
+            </Tab>
+          </TabList>
 
+          <TabPanel>
+            <p>My Order</p>
+          </TabPanel>
+          <TabPanel>
+            <p>Process</p>
+          </TabPanel>
+          <TabPanel>
+            <p>Received</p>
+          </TabPanel>
+          <TabPanel>
+            <p>Received</p>
+          </TabPanel>
+          <TabPanel>
+            <p>Received</p>
+          </TabPanel>
+        </Tabs>
+      </div>
+    </div>
+  )
+}
+
+const Address = () => {
+  return (
+    <div className="wrap-right-content">
+      <div>
+        <h3>Choose another address</h3>
+        <p>Manage your shipping address</p>
+        <hr/>
+      </div>
+      <div className="rc-center">
+        <div id="add-address">
+          <div className="add-new-address mb-4 mt-4">
+            <button>Add new address</button>
+          </div>
+          <div className="address-list selected">
+            <h2>Andreas Jane</h2>
+            <p>Perumahan Sapphire Mediterania, Wiradadi, Kec. Sokaraja, Kabupaten Banyumas, Jawa Tengah, 53181 [Tokopedia Note: blok c 16] Sokaraja, Kab. Banyumas, 53181</p>
+            <button type="button">Change address</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 const MyProfile = ({handleChange, startDate}) => { 
   return(
     <div className="wrap-right-content">
-            <div >
-              <h3>My Profile</h3>
-              <p>Manage your profile information</p>
-              <hr/>
-            </div>
-            <div className="rc-center">
-              <div className="section-1">
-                {/* form */}
-                <div className="sc1-title">
-                  <p>Name</p>
-                  <p>Email</p>
-                  <p>Phone number</p>
-                  <p>Gender</p>
-                  <p>Date of birth</p>
-                </div>
-                <div className="sc1-input">
-                  <p><input type="text"/></p>
-                  <p><input type="text"/></p>
-                  <p><input type="text"/></p>
-                  <p className="sci-radio"><input type="radio" name="gender" value="man"/> <span> Laki-laki </span> {'   '} <input type="radio" name="gender" value="woman"/> <span> Perempuan </span></p>
-                  <div className="date">
-                  {/* <select>
-                    <option value="1">{'1'}</option>
-                    {[1,2,3,4,5,6,7,8].map(d => <option key={d}>{d}</option>)}
-                  </select>  */}
-                  <DatePicker
-                    onChange={handleChange}
-                    selected={startDate}
-                    dateFormat="dd/MM/yyyy"
-                  /> 
-                  {/* <select>
-                    <option value="Januari">{'Januari'}</option>
-                    {['Januari','Februari','Maret','April','Mei','Juni','Juli','Agustus', 'Sepetember', 'Oktober', 'Nopember', 'Desember'].map(m => <option key={m}>{m}</option>)}
-                  </select>
-                  <select>
-                    <option value="1990">{'1990'}</option>
-                    {[1990,1991,1992,1993,1994,1995,1996,1997].map(y => <option key={y}>{y}</option>)}
-                  </select>  */}
-                  </div>
-                </div>
-              </div>
-              <div className="section-foto left-border">
-                {/* foto */}
-                <img src={customer} alt="asdf" width="111" height="111"/>
-                <button type="button" className="mt-3">Select image</button>
-                <input type="file"/>
-              </div> 
-            </div>
-            <div>
-              <div>
-                {/* buton */}
-                <button className="btn btn-secondary">Save</button>
-              </div>
+      <div >
+        <h3>My Profile</h3>
+        <p>Manage your profile information</p>
+        <hr/>
+      </div>
+      <div className="rc-center">
+        <div className="section-1">
+          {/* form */}
+          <div className="sc1-title">
+            <p>Name</p>
+            <p>Email</p>
+            <p>Phone number</p>
+            <p>Gender</p>
+            <p>Date of birth</p>
+          </div>
+          <div className="sc1-input">
+            <p><input type="text"/></p>
+            <p><input type="text"/></p>
+            <p><input type="text"/></p>
+            <p className="sci-radio"><input type="radio" name="gender" value="man"/> <span> Laki-laki </span> {'   '} <input type="radio" name="gender" value="woman"/> <span> Perempuan </span></p>
+            <div className="date">
+            {/* <select>
+              <option value="1">{'1'}</option>
+              {[1,2,3,4,5,6,7,8].map(d => <option key={d}>{d}</option>)}
+            </select>  */}
+            <DatePicker
+              onChange={handleChange}
+              selected={startDate}
+              dateFormat="dd/MM/yyyy"
+            /> 
+            {/* <select>
+              <option value="Januari">{'Januari'}</option>
+              {['Januari','Februari','Maret','April','Mei','Juni','Juli','Agustus', 'Sepetember', 'Oktober', 'Nopember', 'Desember'].map(m => <option key={m}>{m}</option>)}
+            </select>
+            <select>
+              <option value="1990">{'1990'}</option>
+              {[1990,1991,1992,1993,1994,1995,1996,1997].map(y => <option key={y}>{y}</option>)}
+            </select>  */}
             </div>
           </div>
+        </div>
+        <div className="section-foto left-border">
+          {/* foto */}
+          <img src={customer} alt="asdf" width="111" height="111"/>
+          <button type="button" className="mt-3">Select image</button>
+          <input type="file"/>
+        </div> 
+      </div>
+      <div>
+        <div>
+          {/* buton */}
+          <button className="btn btn-secondary">Save</button>
+        </div>
+      </div>
+    </div>
   )
 }
